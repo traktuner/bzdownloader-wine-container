7ca6000bab2f2ea07c995775f9a8772de7028bc6
Thomas Langstadlinger <thomas@langstadlinger.eu>
2022-03-05 17:12:14 +0100

    start correct exe

--- startapp.sh
+++ startapp.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 set -x
-if [ -f "/config/wine/drive_c/Program Files (x86)/Backblaze/bzbui.exe" ]
-then
-    wine64 "/config/wine/drive_c/Program Files (x86)/Backblaze/bzbui.exe" -noqiet &
-    sleep infinity
-else
+ 
     mkdir /config/wine/
     cd /config/wine/
     curl -L "https://secure.backblaze.com/api/restore_downloader" --output "bzdownloader.zip"
     unzip bzdownloader.zip
     ls -la
+    echo "Starting bzdownloader.exe"
+    wine64 "bzdownloader.exe" &
     sleep infinity
-    wine64 "install_backblaze.exe" &
-    sleep infinity
-fi
